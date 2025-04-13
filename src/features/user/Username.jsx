87c3c9d980fe5fb9337dd694dbe7db9a6ef4a82b
zahnx <breadsw01@gmail.com>
2025-04-13 20:21:05 +0800

    Added styles on each component using tailwind css

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold md:block">Razzel</div>;
+}
+
+export default Username;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,21 +1,23 @@
-import { Outlet, useNavigation } from "react-router-dom";
-import CartOverview from "../features/cart/CartOverview";
-import Header from "./Header";
-import Loader from "./Loader";
+import { Outlet, useNavigation } from 'react-router-dom';
+import CartOverview from '../features/cart/CartOverview';
+import Header from './Header';
+import Loader from './Loader';
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  console.log(navigation);
+  const isLoading = navigation.state === 'loading';
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
 
-      <main>
-        <h1>Content</h1>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll overflow-x-hidden">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
